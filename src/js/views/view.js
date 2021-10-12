37cd04c2d432fa4df8fdaf0f7e58de5e30d42ca2
mobak88 <mostafa0401@ymail.com>
2021-10-12 15:18:13 +0200

    Added readme

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -12,6 +12,7 @@ export default class View {
    * @author Jonas Schmedtmann
    * @todo Finish implementation
    */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
