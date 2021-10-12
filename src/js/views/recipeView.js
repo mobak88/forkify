ac9cbbfc892e4346ecbb756ab7fd5257d356da53
mobak88 <mostafa0401@ymail.com>
2021-10-12 15:55:47 +0200

    Fixing bugs

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './view.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-const Fraction = require('fractional').Fraction;
+import { Fraction } from 'fraction.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
